8a9e13419aa06c05e1ee521ed4e959bc480eeba1
PhanTien234 <minhtienp328@gmail.com>
2023-01-04 22:17:13 +0700

    declare Instance Variable

---
+++ Learn CSharp/Variable/InstanceVariable.cs
@@ -0,0 +1,40 @@
+﻿namespace Learn_CSharp.Variable;
+
+public class InstanceVariable
+{
+    // These variables are instance variables.
+    // These variables are in a class and
+    // are not inside any function
+    int engMarks;
+    int mathsMarks;
+    int phyMarks;
+ 
+    // Main Method
+    public static void Main(String[] args)
+    {
+         
+        // first object
+        InstanceVariable obj1 = new InstanceVariable();
+        obj1.engMarks = 90;
+        obj1.mathsMarks = 80;
+        obj1.phyMarks = 93;
+ 
+        // second object
+        InstanceVariable obj2 = new InstanceVariable();
+        obj2.engMarks = 95;
+        obj2.mathsMarks = 70;
+        obj2.phyMarks = 90;
+ 
+        // displaying marks for first object
+        Console.WriteLine("Marks for first object:");
+        Console.WriteLine(obj1.engMarks);
+        Console.WriteLine(obj1.mathsMarks);
+        Console.WriteLine(obj1.phyMarks);
+ 
+        // displaying marks for second object
+        Console.WriteLine("Marks for second object:");
+        Console.WriteLine(obj2.engMarks);
+        Console.WriteLine(obj2.mathsMarks);
+        Console.WriteLine(obj2.phyMarks);
+    }
+}
