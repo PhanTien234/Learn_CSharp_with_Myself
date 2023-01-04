97d36112015223c7c0c40245b934a717bea0b43f
PhanTien234 <minhtienp328@gmail.com>
2023-01-04 22:11:58 +0700

    declare local variable

---
+++ Learn CSharp/Variable/LocalVariable.cs
@@ -0,0 +1,26 @@
+﻿namespace Learn_CSharp.Variable;
+// The local variable
+public class LocalVariable
+{
+    // Method
+    public void StudentAge()
+    {
+         
+        // local variable age
+        int age = 0;
+         
+        age = age + 10;
+        Console.WriteLine("Student age is : " + age);
+    }
+ 
+    // Main Method
+    public static void Main(String[] args)
+    {
+         
+        // Creating object
+        LocalVariable obj = new LocalVariable();
+         
+        // calling the function
+        obj.StudentAge();
+    }
+}
