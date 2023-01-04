a1629c76219529ae71de3b428f4c51f78bcd30af
PhanTien234 <minhtienp328@gmail.com>
2023-01-04 22:26:56 +0700

    Declare Static variable and Constant variable

---
+++ Learn CSharp/Variable/ConstantVariable.cs
@@ -0,0 +1,26 @@
+﻿namespace Learn_CSharp.Variable;
+
+public class ConstantVariable
+{
+    // instance variable
+    int a = 10;
+     
+    // static variable
+    static int b = 20;
+ 
+    // constant variable
+    const float max = 50;
+ 
+    // Main Method
+    public static void Main()
+    {
+         
+        // creating object
+        ConstantVariable obj = new ConstantVariable();
+         
+        // displaying result
+        Console.WriteLine("The value of a is = " + obj.a);
+        Console.WriteLine("The value of b is = " + ConstantVariable.b);
+        Console.WriteLine("The value of max is = " + ConstantVariable.max);
+    }
+}
---
+++ Learn CSharp/Variable/StaticVariable.cs
@@ -0,0 +1,20 @@
+﻿namespace Learn_CSharp.Variable;
+
+public class StaticVariable
+{
+    // static variable salary
+    static double salary;
+    static String name = "Aks";
+ 
+    // Main Method
+    public static void Main(String[] args)
+    {
+ 
+        // accessing static variable
+        // without object
+        StaticVariable.salary = 100000;
+         
+        Console.WriteLine(StaticVariable.name + "'s average salary:"
+                                   + StaticVariable.salary);
+    }
+}
