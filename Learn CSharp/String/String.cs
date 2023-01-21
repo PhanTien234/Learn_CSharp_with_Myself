c6d08732fd40f89f38f082def7e16130ae6420ab
PhanTien234 <minhtienp328@gmail.com>
2023-01-21 19:16:14 +0700

    create string and get the length of string

---
+++ Learn CSharp/String/String.cs
@@ -0,0 +1,19 @@
+﻿namespace Learn_CSharp.String;
+
+public class String{
+    public static void Main(string[] args)
+    {
+        // create string
+        string str1 = "C# Programming";
+        string str2 = "Programiz";
+        //print string 
+        Console.WriteLine(str1);
+        Console.WriteLine(str2);
+
+        Console.ReadLine();
+        
+        // get length of str
+        int length = str1.Length;
+        Console.WriteLine("Length: " + length);
+    }
+}
