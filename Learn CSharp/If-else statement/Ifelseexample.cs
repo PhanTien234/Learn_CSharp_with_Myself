991c978772bec1edc8c365862241c7c9e7c5e62f
PhanTien234 <minhtienp328@gmail.com>
2023-02-21 16:28:23 +0700

    If-else statement

---
+++ Learn CSharp/If-else statement/Ifelseexample.cs
@@ -0,0 +1,17 @@
+﻿namespace Learn_CSharp.If_else_statement;
+
+public static void Main(String[] args)
+{
+    int i= Int32.Parse(Console.ReadLine());
+    if (i < 23)
+    {
+        Console.WriteLine("The number less than 23");
+    }else if (i > 23)
+    {
+        Console.WriteLine("the number more than 23");
+    }
+    else
+    {
+        Console.WriteLine("As as");
+    }
+}
