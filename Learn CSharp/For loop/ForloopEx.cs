77eb1215c48ccb3cea73b7e64bfbf1c32377cfef
PhanTien234 <minhtienp328@gmail.com>
2023-02-21 21:18:19 +0700

    For loop statement

---
+++ Learn CSharp/For loop/ForloopEx.cs
@@ -0,0 +1,10 @@
+﻿namespace Learn_CSharp.For_loop;
+
+public static void Main(String[] args)
+{
+    // Print out numbers from 0 to 5
+    for (int i = 0; i < 6; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
