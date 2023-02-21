4a680db26a288967bae50f22c40d33b379f2b854
PhanTien234 <minhtienp328@gmail.com>
2023-02-21 16:40:25 +0700

    Switch case

---
+++ Learn CSharp/SwitchCaseStatement/SwitchcaseEx.cs
@@ -0,0 +1,34 @@
+﻿namespace Learn_CSharp.SwitchCaseStatement;
+
+public static void Main(String[] args)
+{
+    // Use the day of week to print out screen the day of week
+    int day = int.Parse(Console.ReadLine());
+    switch (day)
+    {
+        case 1:
+            Console.WriteLine("Monday");
+            break;
+        case 2:
+            Console.WriteLine("Tuesday");
+            break;
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+        case 4:
+            Console.WriteLine("Thursday");
+            break;
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+        case 7:
+            Console.WriteLine("Sunday");
+            break;
+        default:
+            Console.WriteLine("Cannot find the day with that number! ");
+            break;
+    }
+}
