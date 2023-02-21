5e409211d4b355ce316b35f00bc2e6fc943f90ef
PhanTien234 <minhtienp328@gmail.com>
2023-02-21 21:22:09 +0700

    foreach statement

---
+++ Learn CSharp/For loop/foreach.cs
@@ -0,0 +1,10 @@
+﻿namespace Learn_CSharp.For_loop;
+
+public static void Main(String[] args)
+{
+    string[] cars = { "Honda", "BWM", "Ford", "Mazda" };
+    foreach (var car in cars)
+    {
+        Console.WriteLine(car);
+    }
+}
