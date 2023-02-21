d296ae65502585d3fe53b83c9343d2641488801e
PhanTien234 <minhtienp328@gmail.com>
2023-02-21 22:38:57 +0700

    do while loop

---
+++ Learn CSharp/Do While loop/dowhile1.cs
@@ -0,0 +1,16 @@
+﻿namespace Learn_CSharp.Do_While_loop;
+
+public class dowhile1
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int sum = 0;
+        do
+        {
+            sum += a;
+            a++;
+        } while (a <= 5); 
+        Console.WriteLine("Sum of 1 to 5 is: " + sum);
+    }
+}
---
+++ Learn CSharp/Do While loop/dowhile2.cs
@@ -0,0 +1,12 @@
+﻿namespace Learn_CSharp.Do_While_loop;
+
+public class dowhile2
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+            Console.WriteLine("While loop endless");
+        } while (true);
+    }
+}
