db4d50b7380130bacd8a847d8b20b4ce69377cdc
PhanTien234 <minhtienp328@gmail.com>
2023-02-21 16:31:59 +0700

    Operator ternary

---
+++ Learn CSharp/If-else statement/Operator Ternary.cs
@@ -0,0 +1,8 @@
+﻿namespace Learn_CSharp.If_else_statement;
+
+public static void Main(String[] args)
+{
+    int i= Int32.Parse(Console.ReadLine());
+    string result = (i < 23) ? "the number less than 23" : "the number more than 23";
+    Console.WriteLine(result);
+}
