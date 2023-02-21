ee02df2aa9f89717d3a398403c12c99532cb232f
PhanTien234 <minhtienp328@gmail.com>
2023-02-21 22:19:06 +0700

    while loop

---
+++ Learn CSharp/While loop/whileloop1.cs
@@ -0,0 +1,14 @@
+﻿namespace Learn_CSharp.While_loop;
+
+public class whileloop1
+{
+    static void Main(string[] args)
+    {
+        int i = 1;
+        while (i<=10)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
---
+++ Learn CSharp/While loop/whileloop2.cs
@@ -0,0 +1,12 @@
+﻿namespace Learn_CSharp.While_loop;
+
+public class whileloop2
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("The while loop will endless");
+        }
+    }
+}
