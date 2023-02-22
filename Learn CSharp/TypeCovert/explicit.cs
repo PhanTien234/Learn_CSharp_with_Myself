19aeb8ad2c751fccb7f3dc7c77bc3e7bd7b36fdf
PhanTien234 <minhtienp328@gmail.com>
2023-02-22 23:17:43 +0700

    type convert (explicit)

---
+++ Learn CSharp/TypeCovert/explicit.cs
@@ -0,0 +1,12 @@
+﻿namespace Learn_CSharp.TypeCovert;
+
+public class expdlicit {
+    public static void Main(string[] args)
+    {
+        int i1 = 300;
+        int i2 = 50;
+        ///
+        byte b1 = (byte)i1;
+        byte b2 = (byte)i2;
+    }
+}
