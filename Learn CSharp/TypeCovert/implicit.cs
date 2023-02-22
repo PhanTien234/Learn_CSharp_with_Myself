f379f67dd756e7dba4739c735669a6bdda6b9db7
PhanTien234 <minhtienp328@gmail.com>
2023-02-22 23:13:06 +0700

    Type convert (implicit)

---
+++ Learn CSharp/TypeCovert/implicit.cs
@@ -0,0 +1,13 @@
+﻿namespace Learn_CSharp.TypeCovert;
+
+public static void Main(string[] args)
+{
+    //TH1: Kieu nho hon (int) => lon hon (long)
+    int intValue1 = 10;
+    long longValue1 = intValue1;
+    Console.WriteLine("Truong hop 1: " + longValue1);
+    //TH2: Kieu lon => nho (se xay ra loi)
+    long longValue2 = 99999999999999;
+    int inValue2 = longValue2;
+    Console.WriteLine(inValue2);
+}
