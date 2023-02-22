40c2a7f5e27356d57fb1e4a2e0443e6ba93d4bb3
PhanTien234 <minhtienp328@gmail.com>
2023-02-22 23:22:53 +0700

    type convert (Parse)

---
+++ Learn CSharp/TypeCovert/parse.cs
@@ -0,0 +1,12 @@
+﻿namespace Learn_CSharp.TypeCovert;
+
+public class parse
+{
+    public static void Main(string[] args)
+    {
+        string value = "69";
+        int number = int.Parse(value);
+        Console.WriteLine("Kieu du lieu ban dau: " + value.GetType());
+        Console.WriteLine("Kieu du lieu ban dau: " + number.GetType());
+    }
+}
