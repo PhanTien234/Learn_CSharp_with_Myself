5192730f80b8770d848cc3c245d0ca518094d06b
PhanTien234 <minhtienp328@gmail.com>
2023-02-22 23:39:13 +0700

    const number

---
+++ Learn CSharp/Const/ConstEX.cs
@@ -0,0 +1,10 @@
+﻿namespace Learn_CSharp.Const;
+
+public class ConstEX
+{
+    public static void Main(string[] args)
+    {
+        // Declare const
+        const string name = "Phan Minh Tien";
+    }
+}
