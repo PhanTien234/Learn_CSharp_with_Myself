446599736206b9a5f0c80299b5432050063e7240
PhanTien234 <minhtienp328@gmail.com>
2023-02-22 23:36:47 +0700

    type convert (Convert 'support class available')

---
+++ Learn CSharp/TypeCovert/Converrt.cs
@@ -0,0 +1,21 @@
+﻿namespace Learn_CSharp.TypeCovert;
+
+public class Converrt
+{
+    public static void Main(string[] args)
+    {
+        int num = "69";
+        string resultTrue1 = "True";
+        string resultTrue2 = "Dung roi";
+        var convert1 = Convert.ToString(num);
+        // Mot cach khac su dung phuong thuc cua kieu du lieu
+        // var convert = num.ToString();
+        var convert2 = Convert.ToBoolean(resultTrue1);
+        
+        //Them dong nay se bi loi
+        var convert3 = Convert.ToBoolean(resultTrue2);
+        Console.WriteLine("Ket qua 1: " + convert1);
+        Console.WriteLine("Ket qua 2: " + convert2);
+        Console.WriteLine("Ket qua 2: " + convert3);
+    }
+}
