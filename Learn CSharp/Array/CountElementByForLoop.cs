b6072628c400e0fa5e651217338e856a4b5a1251
PhanTien234 <minhtienp328@gmail.com>
2023-02-24 22:46:04 +0700

    Count Element by for loop in array

---
+++ Learn CSharp/Array/CountElementByForLoop.cs
@@ -0,0 +1,13 @@
+﻿namespace Learn_CSharp.Array;
+
+public class CountElementByForLoop
+{
+    static void Main(string[] args)
+    {
+        string[] cars = { "Yamaha", "Tesla", "BMW", "Toyata" };
+        for (int i = 0; i < cars.Length; i++)
+        {
+            Console.WriteLine(cars[i]);
+        }
+    }
+}
