2eee19484be7edd936f2ef293f3df22547f9c069
PhanTien234 <minhtienp328@gmail.com>
2023-02-24 22:41:54 +0700

    LengthofArray

---
+++ Learn CSharp/Array/LengthofArray.cs
@@ -0,0 +1,10 @@
+﻿namespace Learn_CSharp.Array;
+
+public class LengthofArray
+{
+    static void Main(string[] args)
+    {
+        string[] cars = { "Honda", "Toyata", "Peague" };
+        Console.WriteLine(cars.Length);
+    }
+}
