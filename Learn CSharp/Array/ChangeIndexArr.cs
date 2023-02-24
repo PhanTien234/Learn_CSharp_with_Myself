922d66915c742fca9a22227c72701cdd50ce20fe
PhanTien234 <minhtienp328@gmail.com>
2023-02-24 22:39:14 +0700

    Change Index Array

---
+++ Learn CSharp/Array/ChangeIndexArr.cs
@@ -0,0 +1,11 @@
+﻿namespace Learn_CSharp.Array;
+
+public class ChangeIndexArr
+{
+    static void Main(string[] args)
+    {
+        string[] car = { "Honda", "Toyata" };
+        car[0] = "morning";
+        Console.WriteLine(car[0]);
+    }
+}
