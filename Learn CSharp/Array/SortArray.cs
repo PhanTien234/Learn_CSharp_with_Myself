71b6dfe6d27866b9214c8d661c459f2544cff3e2
PhanTien234 <minhtienp328@gmail.com>
2023-02-24 22:53:36 +0700

    Use Array.Sort in Array

---
+++ Learn CSharp/Array/SortArray.cs
@@ -0,0 +1,14 @@
+﻿namespace Learn_CSharp.Array;
+
+public class SortArray
+{
+    static void Main(string[] args)
+    {
+        string[] cars = { "Honda", "Toyata", "BMW", "Ford" };
+        System.Array.Sort(cars);
+        for (int i = 0; i < cars.Length; i++)
+        {
+            Console.WriteLine(cars[i]);
+        }
+    }
+}
