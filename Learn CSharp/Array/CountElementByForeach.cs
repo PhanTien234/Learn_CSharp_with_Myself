7fe379ac475711d097eb401760e8aa2b7c47bfef
PhanTien234 <minhtienp328@gmail.com>
2023-02-24 22:49:20 +0700

    Count Element by foreach in array

---
+++ Learn CSharp/Array/CountElementByForeach.cs
@@ -0,0 +1,13 @@
+﻿namespace Learn_CSharp.Array;
+
+public class CountElementByForeach
+{
+    static void Main(string[] args)
+    {
+        string[] cars = { "Honda", "BWM", "Ford", "Mazda" };
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
