ae393631100bdcf7d0c9cd87b9c4f7e78c689f3e
PhanTien234 <minhtienp328@gmail.com>
2023-02-24 23:01:23 +0700

    use some method available of Linq

---
+++ Learn CSharp/Array/LinqInArray.cs
@@ -0,0 +1,15 @@
+﻿namespace Learn_CSharp.Array;
+
+public class LinqInArray
+{
+    static void Main(string[] args)
+    {
+        int[] myNumbers = { 5, 1, 8, 9 };
+        //return to the highest value
+        Console.WriteLine("The highest value: " + myNumbers.Max());
+        //return to the lowest value
+        Console.WriteLine("The lowest value: " + myNumbers.Min());
+        //return to Sum of value
+        Console.WriteLine("Sum of value: " + myNumbers.Sum());
+    }
+}
