8c08e743b224413ca47c95b9927710a60d8dd425
PhanTien234 <minhtienp328@gmail.com>
2023-02-24 22:34:40 +0700

    Example Array

---
+++ Learn CSharp/Array/ExArray.cs
@@ -0,0 +1,10 @@
+﻿namespace Learn_CSharp.Array;
+
+public class ExArray
+{
+    static void Main(string[] args)
+    {
+        string[] cars = { "Honda", "BMW", "Ford", "Mazda" };
+        Console.WriteLine(cars[0]);
+    }
+}
