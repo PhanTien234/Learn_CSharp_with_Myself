67c40c09fab4c41a3beb0736422d4f0b9376c2a6
PhanTien234 <minhtienp328@gmail.com>
2023-02-27 22:25:45 +0700

    Collection Example - Queue

---
+++ Learn CSharp/Collections/Queuee.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace Learn_CSharp.Collections;
+
+public class Queuee
+{
+    static void Main(string[] args)
+    {
+        //Creates and initializes a new Queue
+        Queue myQ = new Queue();
+        myQ.Enqueue("Hello");
+        myQ.Enqueue("World");
+        myQ.Enqueue("!");
+        //Display the properties and values of the Queue
+        Console.WriteLine(myQ);
+        Console.WriteLine("\tCount: {0}", myQ.Count);
+        Console.WriteLine("\tValues: ");
+        foreach (Object obj in myQ)
+        {
+            Console.Write("        {0}", obj);
+        }
+    }
+}
+// Result:
+// Count: 3
+// Values: Hello World !
