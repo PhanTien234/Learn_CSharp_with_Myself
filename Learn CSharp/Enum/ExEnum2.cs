a2d836a4f8a3472e2ebaf3b87e6288098582eb0f
PhanTien234 <minhtienp328@gmail.com>
2023-02-27 20:51:30 +0700

    Enum Example 2, by default the first of enum have the value is 0

---
+++ Learn CSharp/Enum/ExEnum2.cs
@@ -0,0 +1,22 @@
+﻿namespace Learn_CSharp.Enum;
+
+public class ExEnum2
+{
+    enum Months
+    {
+        January, //0
+        February, //1
+        March, //2
+        April, //3
+        May, //4
+        June, //5
+        July //6
+    }
+
+    static void Main(string[] args)
+    {
+        int myNum = (int)Months.April;
+        Console.WriteLine(myNum);
+    }
+}
+//Result: 3
