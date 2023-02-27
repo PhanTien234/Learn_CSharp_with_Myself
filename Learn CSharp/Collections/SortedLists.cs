579ab8ede472908e62bbfb59764d06dbd78d5896
PhanTien234 <minhtienp328@gmail.com>
2023-02-27 22:06:28 +0700

    Collection Example - SortedList

---
+++ Learn CSharp/Collections/SortedLists.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace Learn_CSharp.Collections;
+
+public class SortedLists
+{
+    static void Main(string[] args)
+    {
+        SortedList mySL = new SortedList();
+        mySL.Add("Third","!");
+        mySL.Add("Second","World");
+        mySL.Add("First", "Hello");
+        //Displays the properties and values of the SortedList
+        Console.WriteLine("mySL");
+        Console.WriteLine("Count: {0}", mySL.Count);
+        Console.WriteLine("Capacity: {0}", mySL.Capacity);
+        Console.WriteLine("Keys and Values: ");
+        Console.WriteLine("\t - KEY- \t - VALUE -");
+        for (int i = 0; i < mySL.Count; i++)
+        {
+            Console.WriteLine("\t {0}: \t {1}", mySL.GetKey(i), mySL.GetByIndex(i));
+        }
+    }
+}
