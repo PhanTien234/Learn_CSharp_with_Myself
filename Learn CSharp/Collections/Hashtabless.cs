68c48b5c74db3b2d35cdd881788aeaff0efb3c0a
PhanTien234 <minhtienp328@gmail.com>
2023-02-27 21:42:35 +0700

    Collection Example - Hashtable

---
+++ Learn CSharp/Collections/Hashtabless.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace Learn_CSharp.Collections;
+
+public class Hashtabless
+{
+    static void Main(string[] args)
+    {
+        Hashtable hashtable = new Hashtable();
+        hashtable.Add("Key 1", "Value 1");
+        hashtable.Add("Key 2", "Value 2");
+        Console.WriteLine(hashtable["Key 1"]);
+        foreach (DictionaryEntry item in hashtable)
+        {
+            Console.WriteLine("Key: {0} - value: {1}", item.Key, item.Value);
+        }
+    }
+}
