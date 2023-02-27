7d55a7dc079dfa9a6bbf90eb627d0a68cd87cc00
PhanTien234 <minhtienp328@gmail.com>
2023-02-27 20:38:18 +0700

    Enum example1 define enum into class C#

---
+++ Learn CSharp/Enum/ExEnum.cs
@@ -0,0 +1,16 @@
+﻿namespace Learn_CSharp.Enum;
+
+public class ExEnum
+{
+    enum Level
+    {
+        Low,
+        Medium,
+        High
+    }
+    static void Main(string[] args)
+    {
+        Level myVar = Level.Medium;
+        Console.WriteLine(myVar);
+    }
+}
