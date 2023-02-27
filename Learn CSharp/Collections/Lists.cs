db6b86d6027fb1a0792aa45d11b603ff3b123c04
PhanTien234 <minhtienp328@gmail.com>
2023-02-27 22:37:16 +0700

    Collection Example - List (Compare between ArrayList and List)

---
+++ Learn CSharp/Collections/Lists.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace Learn_CSharp.Collections;
+
+public class Lists
+{
+    // Compare the level of the safety for security between ArrayList and List
+    static void Main(string[] args)
+    {
+        ArrayList arrayList1 = new ArrayList();
+        arrayList1.Add(1);
+        arrayList1.Add("Pony"); //<- No error at complie process
+        int total = 0;
+        foreach (int num in arrayList1)
+        {
+            total += num; //<- Runtime Error
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        List<int> list1 = new List<int>();
+        list1.Add(1);
+        //list1.Add("Pony"); // <-Errol at complie process
+        int total = 0;
+        foreach (int num in list1)
+        {
+            total += num;
+        }
+    }
+    
+    
+}
