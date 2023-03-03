b5ecb71f9fcf17e3afb9290d4873b88a48a6ae60
PhanTien234 <minhtienp328@gmail.com>
2023-03-03 23:24:26 +0700

    Event and Handle Event in Struct

---
+++ Learn CSharp/Structt/EventsAndHandleEventInStructs.cs
@@ -0,0 +1,50 @@
+﻿namespace Learn_CSharp.Structt;
+
+public class EventsAndHandleEventInStructs
+{
+    struct Coordinate
+    {
+        private int _x, _y;
+
+        public int x
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+                CoordinateChanged(_x);
+            }
+        }
+
+        public int y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+                CoordinateChanged(_y);
+            }
+        }
+
+        public event Action<int> CoordinateChanged;
+    }
+
+    static void Main(string[] args)
+    {
+        Coordinate point = new Coordinate();
+        point.CoordinateChanged += StructEventHandler;
+        point.x = 10;
+        point.y = 20;
+    }
+
+    static void StructEventHandler(int point)
+    {
+        Console.WriteLine("Coordinate changed to {0}", point);
+    }
+}
