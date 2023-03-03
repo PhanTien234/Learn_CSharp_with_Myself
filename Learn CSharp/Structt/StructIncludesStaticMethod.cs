66cd9d81e5e4d327de7efd6a2f06f69dfe5eed01
PhanTien234 <minhtienp328@gmail.com>
2023-03-03 23:15:12 +0700

    Static Constructor in Struct

---
+++ Learn CSharp/Structt/StructIncludesStaticMethod.cs
@@ -0,0 +1,28 @@
+﻿namespace Learn_CSharp.Structt;
+
+public class StructIncludesStaticMethod
+{
+    struct Coordinate
+    {
+        public int x;
+        public int y;
+
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Coordinate GetOrigin()
+        {
+            return new Coordinate();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Coordinate point = Coordinate.GetOrigin();
+        Console.WriteLine(point.x); //output: 0
+        Console.WriteLine(point.y); //output: 0
+    }
+}
