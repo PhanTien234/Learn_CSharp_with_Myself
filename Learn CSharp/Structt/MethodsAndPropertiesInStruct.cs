a262e87e90b5106f50deae832a30d5e75b46a9d7
PhanTien234 <minhtienp328@gmail.com>
2023-03-03 23:09:04 +0700

    Properties and Method in Struct

---
+++ Learn CSharp/Structt/MethodsAndPropertiesInStruct.cs
@@ -0,0 +1,24 @@
+﻿namespace Learn_CSharp.Structt;
+
+public class MethodsAndPropertiesInStruct
+{
+    struct Coordinate
+    {
+        public int x { set; get; }
+        public int y { set; get; }
+
+        public void SetOrigin()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Coordinate point = new Coordinate();
+        point.SetOrigin();
+        Console.WriteLine(point.x); //output: 0;
+        Console.WriteLine(point.y); //output: 0;
+    }
+}
