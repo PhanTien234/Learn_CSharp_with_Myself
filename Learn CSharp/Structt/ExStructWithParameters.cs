0699a5468044ec269fa85e4f6942cf8363e7a035
PhanTien234 <minhtienp328@gmail.com>
2023-03-03 23:02:34 +0700

    Example Constructor parametered in Struct

---
+++ Learn CSharp/Structt/ExStructWithParameters.cs
@@ -0,0 +1,23 @@
+﻿namespace Learn_CSharp.Structt;
+
+public class ExStructWithParameters
+{
+    struct Coordinate
+    {
+        public int x;
+        public int y;
+
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Coordinate point = new Coordinate(10, 20);
+        Console.WriteLine(point.x); //output 10
+        Console.WriteLine(point.y); //output 20
+    }
+}
