d75be7f2d703060411755907dc76c08583796b1c
PhanTien234 <minhtienp328@gmail.com>
2023-03-03 22:47:48 +0700

    Create a struct example

---
+++ Learn CSharp/Structt/ExStruct.cs
@@ -0,0 +1,28 @@
+﻿namespace Learn_CSharp.Structt;
+
+public class ExStruct
+{
+    struct Coordinate
+            {
+                public int x;
+                public int y;
+            }
+    // Create structure without new keyword
+    static void Main(string[] args)
+    {
+        Coordinate point;
+        Console.Write(point.x); //Compile time error
+        point.x = 10;
+        point.y = 20;
+        Console.Write(point.x); // output: 10;
+        Console.Write(point.y); // output: 20;
+    }
+    //create struct with new keyword
+    static void Main(string[] args)
+    {
+        Coordinate point = new Coordinate();
+        Console.WriteLine(point.x); //output: 0
+        Console.WriteLine(point.y); //output: 0
+    }
+}
+  
