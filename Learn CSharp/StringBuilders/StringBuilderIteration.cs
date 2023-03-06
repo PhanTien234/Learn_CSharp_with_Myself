c3debfb848c40d1e4c58405a73b30876f1703197
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 22:50:48 +0700

    StringBuilder - Iteration

---
+++ Learn CSharp/StringBuilders/StringBuilderIteration.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace Learn_CSharp.StringBuilders;
+
+public class StringBuilderIteration
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder("Hello World!!");
+        for(int i = 0; i < sb.Length; i++)
+            Console.Write(sb[i]);
+    }
+    
+}
+//The result: Hello World!!
