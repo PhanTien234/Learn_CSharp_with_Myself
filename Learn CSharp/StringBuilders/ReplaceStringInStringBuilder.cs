b45a8c4275caf3f72617079153b8152a0f5fa1c0
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 23:16:22 +0700

    StringBuilder - Replace String in StringBuilder

---
+++ Learn CSharp/StringBuilders/ReplaceStringInStringBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace Learn_CSharp.StringBuilders;
+
+public class ReplaceStringInStringBuilder
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder("Hello World!!", 50);
+        sb.Replace("World", "C#");
+        Console.WriteLine(sb);
+    }
+}
+// The result: Hello C#!!
