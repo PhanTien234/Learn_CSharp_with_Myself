c2fd8094bb8b20b3ed36ad5719bd1c6d5e9fcd94
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 22:47:00 +0700

    StringBuilder - Create StringBuilder

---
+++ Learn CSharp/StringBuilders/ExCreateStringBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Learn_CSharp.StringBuilders;
+
+public class ExCreateStringBuilder
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder(); //string will be appended later
+        //or
+        StringBuilder sb = new StringBuilder("Hello World!");
+        // or use overloaded constructors
+        StringBuilder sb = new StringBuilder(50); // string will be appended later
+        // or
+        StringBuilder sb = new StringBuilder("Hello World!", 50);
+
+    }
+}
