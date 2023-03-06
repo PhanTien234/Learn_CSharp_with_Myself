64ce91bf1a4f7ac30889115902f46d3ad28aba8f
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 23:06:51 +0700

    StringBuilder - Insert String Into StringBuilder

---
+++ Learn CSharp/StringBuilders/InsertStringIntoStringBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace Learn_CSharp.StringBuilders;
+
+public class InsertStringIntoStringBuilder
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder("Hello World!", 50);
+        sb.Insert(5, " C#");
+        Console.WriteLine(sb);
+    }
+}
+//The result: Hello C# World!
