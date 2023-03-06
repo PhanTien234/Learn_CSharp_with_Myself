c927bd608df3fc9954350be47c788bf1adb3fb44
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 21:59:01 +0700

    DateTime - Substract

---
+++ Learn CSharp/DateTimes/SubstractDates.cs
@@ -0,0 +1,12 @@
+﻿namespace Learn_CSharp.DateTimes;
+
+public class SubstractDates
+{
+    static void Main(string[] args)
+    {
+        DateTime dt1 = new DateTime(2015, 12, 31);
+        DateTime dt2 = new DateTime(2016, 2, 2);
+        TimeSpan result = dt2.Subtract(dt1);
+        Console.WriteLine(result);
+    }
+}
