648bd6dc0018ed0c4f8b2c5aa69950b3eefe518f
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 23:03:18 +0700

    StringBuilder - AppendFormatmethod

---
+++ Learn CSharp/StringBuilders/AppendFormatmethod.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace Learn_CSharp.StringBuilders;
+
+public class AppendFormatmethod
+{
+    static void Main(string[] args)
+    {
+        StringBuilder amountMsg = new StringBuilder("Your total amount is ");
+        amountMsg.AppendFormat("{0:C} ", 25);
+        Console.WriteLine(amountMsg);
+    }
+}
