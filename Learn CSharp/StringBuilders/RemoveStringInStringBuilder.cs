157c3ced6909b0a79e214c2d361dc821098ca2c5
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 23:10:21 +0700

    StringBuilder - Remove String in StringBuilder

---
+++ Learn CSharp/StringBuilders/RemoveStringInStringBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace Learn_CSharp.StringBuilders;
+
+public class RemoveStringInStringBuilder
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder("Hello World!", 50);
+        sb.Remove(6, 7);
+        Console.WriteLine(sb);
+    }
+}
+//The result: Hello
