3db95ff2c138743c8355bc979d1c2a88e8611a05
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 22:59:06 +0700

    StringBuilder - Add/Append String To StringBuilder

---
+++ Learn CSharp/StringBuilders/AddAppendStringToStringBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Learn_CSharp.StringBuilders;
+
+public class AddAppendStringToStringBuilder
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("World!");
+        sb.AppendLine("Hello C#!");
+        sb.AppendLine("This is new line. ");
+        Console.WriteLine(sb);
+    }
+}
+//The result:
+//Hello World! Hello C#!
+//This is new line
