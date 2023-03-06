b5067c9579845c06f30c760f94c1ab51658ac0e0
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 22:23:16 +0700

    DateTime - Convert String to DateTime

---
+++ Learn CSharp/DateTimes/ConvertStringToDateTime.cs
@@ -0,0 +1,19 @@
+﻿namespace Learn_CSharp.DateTimes;
+
+public class ConvertStringToDateTime
+{
+    static void Main(string[] args)
+    {
+        var str = "5/12/2020";
+        DateTime dt;
+        var isValidDate = DateTime.TryParse(str, out dt);
+        if(isValidDate)
+            Console.WriteLine(dt);
+        else
+        {
+            Console.WriteLine($"{str} is not a valid date string");
+        }
+    }
+}
+
+// The result: 5/12/2020 12:00:00 AM
