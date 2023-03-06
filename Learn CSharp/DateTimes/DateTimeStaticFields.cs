cdff49cc0d02a76cf7e5623f05a415db0a823c30
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 21:51:08 +0700

    DateTime - Static Fields

---
+++ Learn CSharp/DateTimes/DateTimeStaticFields.cs
@@ -0,0 +1,19 @@
+﻿namespace Learn_CSharp.DateTimes;
+
+public class DateTimeStaticFields
+{
+    static void Main(string[] args)
+    {
+        DateTime currentDateTime = DateTime.Now;
+        DateTime todaysDate = DateTime.Today;
+        DateTime currentDateTimeUTC = DateTime.UtcNow;
+
+        DateTime maxDateTimeValue = DateTime.MaxValue;
+        DateTime minDateTimeValue = DateTime.MinValue;
+        Console.WriteLine($"Current DateTime {currentDateTime}");
+        Console.WriteLine($"Today's DateTime {todaysDate}");
+        Console.WriteLine($"Current DateTime UTC Timezone {currentDateTimeUTC}");
+        Console.WriteLine($"Max DateTime Value {maxDateTimeValue}");
+        Console.WriteLine($"Min DateTime Value {minDateTimeValue}");
+    }
+}
