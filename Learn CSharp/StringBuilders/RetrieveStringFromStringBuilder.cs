b863d9741006de7b045202ae64b4e3115ae1ab01
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 22:54:15 +0700

    StringBuilder - Retrieve String from StringBuilder

---
+++ Learn CSharp/StringBuilders/RetrieveStringFromStringBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace Learn_CSharp.StringBuilders;
+
+public class RetrieveStringFromStringBuilder
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder("Hello World!");
+        var greet = sb.ToString();
+        Console.WriteLine(greet);
+    }
+}
