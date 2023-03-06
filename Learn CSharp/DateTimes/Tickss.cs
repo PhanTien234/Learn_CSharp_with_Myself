1227b990043ca91fa802e964e0bf76e4a35c2f92
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 21:13:42 +0700

    DateTime - Ticks

---
+++ Learn CSharp/DateTimes/Tickss.cs
@@ -0,0 +1,12 @@
+﻿namespace Learn_CSharp.DateTimes;
+
+public class Tickss
+{
+    static void Main(string[] args)
+    {
+        DateTime dt = new DateTime(63637000000000000);
+        Console.WriteLine(dt);
+        Console.WriteLine(DateTime.MinValue.Ticks); //min value of ticks
+        Console.WriteLine(DateTime.MaxValue.Ticks); //max value of ticks
+    }
+}
