2eeacc1bbab37a6140855ca98406bd7aee579b87
PhanTien234 <minhtienp328@gmail.com>
2023-03-06 21:55:20 +0700

    DateTime - TimeSpan

---
+++ Learn CSharp/DateTimes/Timespans.cs
@@ -0,0 +1,13 @@
+﻿namespace Learn_CSharp.DateTimes;
+
+public class TimeSpans
+
+{
+    static void Main(string[] args)
+    {
+        DateTime dt = new DateTime(2015, 12, 31);
+        TimeSpan ts = new TimeSpan(25, 20, 55);
+        DateTime newDate = dt.Add(ts);
+        Console.WriteLine(newDate);
+    }
+}
