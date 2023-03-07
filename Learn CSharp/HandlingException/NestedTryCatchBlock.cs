8b8841955c116ca3efff51957a3ef66b30ad43d9
PhanTien234 <minhtienp328@gmail.com>
2023-03-07 21:50:37 +0700

    Handling Exception - Nested Try Catch Block

---
+++ Learn CSharp/HandlingException/NestedTryCatchBlock.cs
@@ -0,0 +1,30 @@
+﻿namespace Learn_CSharp.HandlingException;
+
+public class NestedTryCatchBlock
+{
+    static void Main(string[] args)
+    {
+        Student std = null;
+        var divider = 0;
+        try
+        {
+            try
+            {
+                var result = 100 / divider;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Inner catch");
+            }
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Outer catch");
+        }
+    }
+}
+
+public class Student
+{
+    public string StudentName { get; set; }
+}
