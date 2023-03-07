9dabdf8bb3e9ba3a38bc87803eb23f9e3f3e0b3a
PhanTien234 <minhtienp328@gmail.com>
2023-03-07 17:26:23 +0700

    Dictionary - Update Dictionary Element

---
+++ Learn CSharp/Dictionarys/UpdateDictionaryElements.cs
@@ -0,0 +1,30 @@
+﻿namespace Learn_CSharp.Dictionarys;
+
+public class UpdateDictionaryElements
+{
+    static void Main(string[] args)
+    {
+        var cities = new Dictionary<string, string>()
+        {
+            { "UK", "London, Manchester, Birmingham" },
+            { "USA", "Chicago, New York, Washington" },
+            { "India", "Mumbai, New Delhi, Pune" }
+        };
+        cities["UK"] = "Liverpool, Bristol"; //update value of UK key
+        cities["USA"] = "Los Angeles, Boston"; //update value of USA key
+        //cities["France"] = "Paris"; //Throws run-time exception: KeyNotFoundException
+        if (cities.ContainsKey("France"))
+        {
+            cities["France"] = "Paris";
+        }
+
+        foreach (var kvp in cities)
+        {
+            Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+        }
+    }
+}
+//The result:
+//Key: USA, Value: Liverpool, Bristol
+//Key: USA, Value: Los Angeles, Boston
+//Key: India, Value: Mumbai, New Delhi, Pune
