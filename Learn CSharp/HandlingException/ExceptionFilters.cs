621ae807b54d73dea5820f3c0cad35034e27fd20
PhanTien234 <minhtienp328@gmail.com>
2023-03-07 21:33:15 +0700

    Handling Exception - Exception Filters (Specific Exception in Catch Block)

---
+++ Learn CSharp/HandlingException/ExceptionFilters.cs
@@ -0,0 +1,31 @@
+﻿namespace Learn_CSharp.HandlingException;
+
+public class ExceptionFilters
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a number to divide 100: ");
+        try
+        {
+            int num = int.Parse(Console.ReadLine());
+            int result = 100 / num;
+            Console.WriteLine("100/{0} = {1}", num, result);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Cannot divide by zero. Please try again.");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine("Invalid operation. Please try again");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Not a valid format. Please try again.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occured! Please try again.");
+        }
+    }
+}
