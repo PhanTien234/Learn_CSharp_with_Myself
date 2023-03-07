5dd378a68e89445dc2930bc84f3968fa9cf828b6
PhanTien234 <minhtienp328@gmail.com>
2023-03-07 21:12:22 +0700

    Handling Exception - Using Try-Catch Block without Exception in Catch block

---
+++ Learn CSharp/HandlingException/ExceptionHandlingUsingTryCatch without ExceptionInCatchBlocks.cs
@@ -0,0 +1,22 @@
+﻿namespace Learn_CSharp.HandlingException;
+
+public class ExceptionHandlingUsingTryCatch_without_ExceptionInCatchBlocks
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter a number: ");
+            var num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Square of {num} is {num * num}");
+        }
+        catch
+        {
+            Console.Write("Error occurred");
+        }
+        finally
+        {
+            Console.Write("Re-try with a different number");
+        }
+    }
+}
