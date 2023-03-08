71a354ee6e88be3c0aede9f94b15077d7621de25
PhanTien234 <minhtienp328@gmail.com>
2023-03-08 17:35:48 +0700

    FileInfo - Write texts to file using StreamWriter

---
+++ Learn CSharp/FileInfoClass/WriteTextsToFileUSingStreamWriter.cs
@@ -0,0 +1,16 @@
+﻿namespace Learn_CSharp.FileInfoClass;
+
+public class WriteTextsToFileUSingStreamWriter
+{
+    static void Main(string[] args)
+    {
+        //Create object of FIleInfo for specified path
+        FileInfo fi = new FileInfo(@"D:\DummyFile.txt");
+        //Open file for Read/Write
+        FileStream fs = fi.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+        //Create StreamWriter object to write string to FileStream
+        StreamWriter sw = new StreamWriter(fs);
+        sw.WriteLine("Another line from streamwriter");
+        sw.Close();
+    }
+}
