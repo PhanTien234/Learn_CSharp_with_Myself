9a4a0ab77393ba60a2ed30557dcc7c70c5104469
PhanTien234 <minhtienp328@gmail.com>
2023-03-08 17:30:13 +0700

    FileInfo - Read file using StreamReader

---
+++ Learn CSharp/FileInfoClass/ReadFileUSingStreamReader.cs
@@ -0,0 +1,19 @@
+﻿namespace Learn_CSharp.FileInfoClass;
+
+public class ReadFileUSingStreamReader
+{
+    static void Main(string[] args)
+    {
+        //Create object of FileInfo for specified path
+        FileInfo fi = new FileInfo(@"D:\DummyFile.txt");
+        //OPen file for Read/Write
+        FileStream fs = fi.Open(FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
+        //Create object of StreamReader by passing FileStream object on which it needs to operates on
+        StreamReader sr = new StreamReader(fs);
+        //Use ReadToEnd method to read all the content from file
+        string fileContent = sr.ReadToEnd();
+        //Close Streamreader object after operation
+        sr.Close();
+        fs.Close();
+    }
+}
