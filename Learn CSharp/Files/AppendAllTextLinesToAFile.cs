52c9d9c9309ea32f46fa1a1acba6a593c83715a6
PhanTien234 <minhtienp328@gmail.com>
2023-03-08 16:39:03 +0700

    File - Append All Text Lines

---
+++ Learn CSharp/Files/AppendAllTextLinesToAFile.cs
@@ -0,0 +1,13 @@
+﻿namespace Learn_CSharp.Files;
+
+public class AppendAllTextLinesToAFile
+{
+    static void Main(string[] args)
+    {
+        string dummyLines = "This is first line." + Environment.NewLine +
+                            "This is second line." + Environment.NewLine +
+                            "This is third line.";
+        //Opens DummyFile.txt and append lines. If file is not exists then create and open.
+        File.AppendAllLines(@"C:\DummyFile.txt", dummyLines.Split(Environment.NewLine.ToCharArray()).ToList<string>());
+    }
+}
