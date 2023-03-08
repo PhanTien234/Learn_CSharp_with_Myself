3539c6d5c05dc4e2bd1ccea393e571742088b1ab
PhanTien234 <minhtienp328@gmail.com>
2023-03-08 17:45:00 +0700

    FileInfo - StreamReader and StreamWriter

---
+++ Learn CSharp/FileInfoClass/StreamReaderAndStreamWrite.cs
@@ -0,0 +1,27 @@
+﻿namespace Learn_CSharp.FileInfoClass;
+
+public class StreamReaderAndStreamWrite
+{
+    static void Main(string[] args)
+    {
+        //Create FileInfo object for DummyFile.txt
+        FileInfo fi = new FileInfo(@"D:\DummyFile.txt");
+        //Open DummyFile.txt for read operation
+        FileStream fsToRead = fi.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+        //Open DummyFile.txt for write operation
+        FileStream fsToWrite = fi.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+        //get the StreamReader
+        StreamReader sr = new StreamReader(fsToRead);
+        //read all texts using StreamReader object
+        string fileContent = sr.ReadToEnd();
+        sr.Close();
+        //get the StreamWriter
+        StreamWriter sw = new StreamWriter(fsToWrite);
+        //write some text using StreamWriter
+        sw.WriteLine("Another line from streamwriter");
+        sw.Close();
+        //Close all Stream objects
+        fsToRead.Close();
+        fsToWrite.Close();
+    }
+}
